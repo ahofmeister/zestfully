ca758c0ffac8ab757525f8a4e37f22ef3652add3
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2025-01-07 07:08:02 +0100

    fix: Stop clicking on shopping list after deleting

--- app/shopping-list/delete-shopping-list.tsx
+++ app/shopping-list/delete-shopping-list.tsx
@@ -1,17 +1,20 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { deleteShoppingList } from "@/app/shopping-list/shopping-list-actions";
-import {Trash2} from "lucide-react";
+import { Trash2 } from "lucide-react";
 
 export function DeleteShoppingList(props: { id: string }) {
   return (
-      <Button
-          variant="destructive"
-          size="iconSm"
-          onClick={() => deleteShoppingList(props.id)}
-          aria-label="Delete list"
-      >
-        <Trash2 className="h-4 w-4" />
-      </Button>
+    <Button
+      variant="destructive"
+      size="iconSm"
+      onClick={(event) => {
+        event.stopPropagation();
+        return deleteShoppingList(props.id);
+      }}
+      aria-label="Delete list"
+    >
+      <Trash2 className="h-4 w-4" />
+    </Button>
   );
 }
