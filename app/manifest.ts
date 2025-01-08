2461e90ece23b62b4cfa7ccb44caf4d0ee020b6a
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2025-01-08 12:24:10 +0100

    feat: Initial PWA

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Zestfully",
+    short_name: "Zestfully",
+    description: "Simple meals made zestfully perfect",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#030303",
+    theme_color: "#030303",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
