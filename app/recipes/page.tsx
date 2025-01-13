d647e95b2c2cfd2949fbbb63e0d8e1b336ec3e5c
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2025-01-13 08:17:03 +0100

    feat: Implement recipe search

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,27 +1,36 @@
-import React from 'react';
-import {createClient} from "@/utils/supabase/server";
-import {Button} from "@/components/ui/button";
+import React from "react";
+import { createClient } from "@/utils/supabase/server";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import {PlusIcon} from "lucide-react";
-import {RecipeCard} from "@/app/recipes/recipe-card";
+import { PlusIcon } from "lucide-react";
+import { RecipeCard } from "@/app/recipes/recipe-card";
+import { RecipeSearchBar } from "@/app/recipes/recipe-search-bar";
 
-const RecipesPage = async () => {
-    const supabase = await createClient();
-    const {data: recipes} = await supabase.from("recipe").select("*")
-    return (
-        <div>
-            <Link href={"/recipes/new"}>
-                <Button size={"sm"}>
-                    <PlusIcon/>
-                </Button>
-            </Link>
-            <div className={"grid grid-cols-2 gap-2 mt-4 sm:grid-cols-3"}>
-                {recipes?.map((recipe) => (
-                    <RecipeCard recipe={recipe}/>
-                ))}
-            </div>
-        </div>
-    );
-};
+export default async function RecipesPage(props: {
+  searchParams: Promise<{
+    q: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const supabase = await createClient();
+    let query = supabase.from("recipe").select("*");
 
-export default RecipesPage;
+    if (searchParams.q) {
+        query = query.textSearch("name", searchParams.q, {config: 'english', type: 'websearch'});
+    }
+
+    const { data: recipes, error } = await query;
+  return (
+    <div className={"flex flex-col gap-y-4 p-1"}>
+      <Link href={"/recipes/new"}>
+        <Button size={"sm"}>
+          <PlusIcon />
+        </Button>
+      </Link>
+      <RecipeSearchBar value={searchParams.q} />
+      <div className={"grid grid-cols-2 gap-2 mt-4 sm:grid-cols-3"}>
+        {recipes?.map((recipe) => <RecipeCard recipe={recipe} />)}
+      </div>
+    </div>
+  );
+}
---
+++ app/recipes/recipe-search-bar.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import useUpdateQueryParams from "@/app/use-update-query-params";
+
+export function RecipeSearchBar(props: { value: string }) {
+    const [searchTerm, setSearchTerm] = useState<string | undefined>(props.value ?? "");
+
+  const stuff = useUpdateQueryParams();
+
+  return (
+    <Input
+      type={"search"}
+      value={searchTerm}
+      onChange={(event) => {
+        let newValue = event.target.value;
+        stuff({ value: newValue, key: "q" });
+        return setSearchTerm(newValue);
+      }}
+    />
+  );
+}
