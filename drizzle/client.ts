ec19fd695cfce1171d63b95f45403388592e40b5
Alexander Hofmeister <HofmeisterAlexander@googlemail.com>
2026-07-20 10:58:44 +0200

    debug: log db url

--- drizzle/client.ts
+++ drizzle/client.ts
@@ -21,6 +21,7 @@ let postgresSqlClient: ReturnType<typeof postgres>;
 
 const databaseUrl = process.env.DATABASE_URL;
 
+console.log(databaseUrl);
 if (!databaseUrl) {
 	throw new Error("DATABASE_URL environment variable is not set");
 }
